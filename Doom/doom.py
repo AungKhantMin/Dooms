c79b1363250732837da98ecd5df1ba7a421c0156
Hades <you@example.comcd>
2019-07-20 21:55:58 +0630

    Add Test Case

---
+++ Doom/doom.py
@@ -0,0 +1,14 @@
+from network import *
+
+if __name__ == '__main__':
+    try:
+        n = Network()
+        x = n.pingsweepNetwork('192.168.43.0')
+        print(x)
+        for host in x:
+            port = n.portScan(host,n.commonPort)
+            n.nmapScan(host,port)
+    except KeyboardInterrupt:
+        exit(0)
+    except:
+        pass
