9a59d7916e3a096bdbf0cb5689cce9a32c3ff79d
Hades <you@example.comcd>
2019-07-21 01:14:33 +0630

    create a interactive shell

--- Doom/doom.py
+++ Doom/doom.py
@@ -1,17 +1,1 @@
 from lib.network import *
-import argparse
-
-
-if __name__ == '__main__':
-    try:
-        n = Network()
-        x = n.pingsweepNetwork('192.168.43.0')
-        print(x)
-        for host in x:
-            port = n.portScan(host,n.commonPort)
-            print(port)
-            print(n.nmapScan(host,port))
-    except KeyboardInterrupt:
-        exit(0)
-    except:
-        pass
