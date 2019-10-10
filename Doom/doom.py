3501499bce0676db71272db6317a933c95214a2e
Aung Khant Min <aungkhantmin.src@gmail.com>
2019-10-11 02:05:01 +0630

    Finish NMAP and PORT Scanning Script
    Need to add Common Service Analysis Scripts

---
+++ Doom/doom.py
@@ -0,0 +1,21 @@
+import sys
+from module.nmap import Nmap
+from module.smb_vuln import *
+
+def doom():
+    open_port = Nmap.multiPortScan(sys.argv[1])
+    nmap_result = Nmap.defaultScan()
+    if 455 in open_port:
+        # call SMB related module
+        pass
+
+    if 80 or 443 in open_port:
+        # call Web related module
+        pass
+
+    if 21 in open_port:
+        # call FTP Service Module
+        pass
+
+    
+
