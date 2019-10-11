6d11c0c8a23d21459ba4c816cf4516977626ff44
Aung Khant Min <aungkhantmin.src@gmail.com>
2019-10-12 04:48:29 +0630

    add some comment for smb file enumeration

--- Doom/module/smb_enum.py
+++ Doom/module/smb_enum.py
@@ -11,43 +11,35 @@ from impacket import  LOG
 
 
 class SMBEnum(object):
-    def __init__(self,ip):
+
+    def __init__(self,ip,user="Guest",password=""):
         self.target = ip
         self.port = 445
-        self.user = "Guest"
-        self.password = ""
+        self.user = user
+        self.password = password
         self.ignore_share = ["IPC$"]
         logger.init()
 
 
 
     def tryLogin(self):
+        '''
+            Setup a connection with smb server, authenticate using the give user name and password. if the user name and
+            password is not specify use guest as login. If successfully authenticated create an interactive section between
+            smb server and client
+        :return:
+        '''
+
         try:
+
+            # Setup SMB Connection and Create an interactive shell
+
             logging.info("Trying To Authenticate As %s ..." % self.user)
-            smbClient = SMBConnection(self.target,self.target,sess_port=self.port)
-            smbClient.login(self.user,self.password)
+            self.smbClient = SMBConnection(self.target,self.target,sess_port=self.port)
+            self.smbClient.login(self.user,self.password)
             logging.info("Successfully Login As %s ..." % self.user)
-            self.shell = MiniImpacketShell(smbClient)
-            shares = self.shell.do_shares("shares")
-            shares_list = []
+            self.shell = MiniImpacketShell(self.smbClient)
 
-            logging.info("Listing Shares")
-            logging.getLogger().level = logging.DEBUG
-
-            for i in range(len(shares)):
-                share = shares[i]['shi1_netname'][:-1]
-                logging.debug(share)
-                if share not in self.ignore_share:
-                    if "$" not in share:
-                        shares_list.append(share)
-
-            LOG.level = logging.INFO
-            for share in shares_list:
-                LOG.info("Listing File And Directory For Share %s \n" % share)
-                self.shell.do_use(share)
-                directory_list = self.shell.do_ls('')
-                print("\n")
-                self.recursive_dirlist(directory_list,share)
 
         except Exception as e:
             LOG.level = logging.DEBUG
@@ -57,23 +49,70 @@ class SMBEnum(object):
             logging.error(str(e))
 
     def recursive_dirlist(self,directory_list,share=""):
+        '''
+            List for file and directory recursively according to their permission.
+        :param directory_list:
+        :param share:
+        :return:
+        '''
 
-            if directory_list is not None:
-                for directory in directory_list:
-                    LOG.info("Listing File And Directory For Path \\\\%s\\%s" % (share, directory))
-                    print("\n")
-                    self.shell.do_cd(directory)
+        if len(directory_list) != 0:
+            for directory in directory_list:
+                LOG.info("Listing File And Directory For Path \\\\%s\\%s" % (share, directory))
+                print("\n")
+                self.shell.do_cd(directory)
+                directory_list = self.shell.do_ls('')
+                print("\n")
+                temp_share = share
+                if len(directory_list) != 0 :
+                    share += "\\" +directory
+                    self.recursive_dirlist(directory_list,share)
+                else:
+                    self.shell.do_cd("..")
+                    share = temp_share
+
+    def enumerateShares(self):
+        '''
+
+        :return:
+        '''
+
+        shares = self.shell.do_shares("shares")
+        shares_list = []
+        logging.info("Listing Shares")
+        logging.getLogger().level = logging.DEBUG
+        for i in range(len(shares)):
+            share = shares[i]['shi1_netname'][:-1]
+            logging.debug(share)
+            if share not in self.ignore_share:
+                if self.smbClient.isGuestSession() > 0:
+                    if "$" not in share:
+                        shares_list.append(share)
+                else:
+                    shares_list.append(share)
+
+        LOG.level = logging.INFO
+        print("\n")
+
+        if len(shares_list) != 0:
+            for share in shares_list:
+                try:
+                    LOG.info("Listing File And Directory For Share %s \n" % share)
+                    self.shell.do_use(share)
                     directory_list = self.shell.do_ls('')
                     print("\n")
-                    temp_share = share
-                    if len(directory_list) != 0 :
-                        share += "\\" +directory
+                    if "$" not in share:
                         self.recursive_dirlist(directory_list,share)
-                    else:
-                        self.shell.do_cd("..")
-                        share = temp_share
+
+                except Exception as e:
+                    LOG.level = logging.DEBUG
+                    if logging.getLogger().level == logging.DEBUG:
+                        import traceback
+                        #traceback.print_exc()
+                    logging.error(str(e))
+                    print("\n")
 
 
-
-gg  = SMBEnum("10.10.10.134")
+gg  = SMBEnum("10.10.10.134")#,user="Administrator",password="thXLHM96BeKL0ER2")
 gg.tryLogin()
+gg.enumerateShares()
