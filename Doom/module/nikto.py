74057b1e3b919edf29da6bdb68ba29f236e578a2
Aung Khant Min <mr.k0ll3r1@gmail.com>
2019-10-13 21:37:22 +0630

    fix

--- Doom/module/nikto.py
+++ Doom/module/nikto.py
@@ -7,13 +7,13 @@ from impacket import LOG
 
 
 class Nikto(object):
-    def __init__(self, ip, portNum):
+    def __init__(self, ip, port="80"): # This is assigning the default param
         self.target = ip
-
+        self.port = port
 
     def niktoScan(self):
         output = \
-            subprocess.Popen(["nikto.pl", "-h", "portNum"], stdout=subprocess.PIPE).communicate()[0]
+            subprocess.Popen(["nikto.pl", "-h", self.target, "-p" , self.port], stdout=subprocess.PIPE).communicate()[0]
         return output
 
 
